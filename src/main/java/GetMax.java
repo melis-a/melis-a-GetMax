df4aa0a106174715fa494ade970e6313fd791679
melis-a <rampersaudmelisa@gmail.com>
2023-03-07 19:47:15 +0000

    max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,18 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+    
+       int max = arr[0];
+       
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i]> 0 && arr[i]> max){
+                max = arr[i];
+            }else if(arr[i]< 0 && arr[i]> max){
+                max = arr[i];
+            } 
+            }
+
+        
+        return max;
     }
 }
